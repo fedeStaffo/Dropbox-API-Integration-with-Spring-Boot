b2f8c837bb8b36b570ad99355c71f9078f6002e1
PierandreaPorfiri <91202391+PierandreaPorfiri@users.noreply.github.com>
2021-12-28 19:01:29 +0100

    Update GetInfoFromJSON.java
    Added parameters: date, shared

--- project/src/main/java/com/progettoOOP/project/utility/GetInfoFromJSON.java
+++ project/src/main/java/com/progettoOOP/project/utility/GetInfoFromJSON.java
@@ -16,7 +16,7 @@ import org.json.simple.parser.ParseException;
 
 public class GetInfoFromJSON {
 
-	public static ArrayList getInfo() {
+	public ArrayList getInfo() {
 		
 		ArrayList<FileModel> lista = new ArrayList<>();
 		
@@ -34,7 +34,7 @@ public class GetInfoFromJSON {
 					+ "    \"recursive\": true,\r\n" //true in quanto permette di vedere le sottocartelle
 					+ "    \"include_media_info\": false,\r\n" 
 					+ "    \"include_deleted\": false,\r\n"
-					+ "    \"include_has_explicit_shared_members\": false,\r\n"
+					+ "    \"include_has_explicit_shared_members\": true,\r\n"
 					+ "    \"include_mounted_folders\": true,\r\n" 
 					+ "    \"include_non_downloadable_files\": true\r\n"
 					+ "}";
@@ -55,7 +55,6 @@ public class GetInfoFromJSON {
 
 				while ((line = buf.readLine()) != null) {
 					data += line;
-					System.out.println(line);
 				}
 			} finally {
 				in.close();
@@ -70,6 +69,9 @@ public class GetInfoFromJSON {
 				String tag = (String) obj2.get(".tag");
 				String name = (String) obj2.get("name");
 				String extension;
+				Number size;
+				String date;
+				boolean shared;
 				
 				if (!(tag.equals("folder"))) {
 					
@@ -83,18 +85,22 @@ public class GetInfoFromJSON {
 					}
 					
 					size = (Number) obj2.get("size");
+					date = (String) obj2.get("server_modified);
+					shared = (boolean) obj2.get("has_explicit_shared_members");			 
+								 
 				} 
 				else // setta come vuoti i parametri che non coincidono tra file e folder
 				{
 					extension = "null";
 					size = 0;
+					date = "null";
+					shared = false;	
 				}
 				
 				String path = (String) obj2.get("path_display");
 				String id = (String) obj2.get("id");
-				Number size = (Number) obj2.get("size");
 				
-				FileModel file = new FileModel(tag, name, extension, path, id, size);
+				FileModel file = new FileModel(tag, name, extension, path, id, size, date, shared);
 				lista.add(file);
 			}
 		} catch (IOException | ParseException e) {
