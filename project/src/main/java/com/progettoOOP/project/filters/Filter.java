1c4ff3eca2133c733bc63183b424ad740abe09ec
PierandreaPorfiri <91202391+PierandreaPorfiri@users.noreply.github.com>
2022-01-05 18:41:48 +0100

    Update Filter.java
    Added Javadoc

--- project/src/main/java/com/progettoOOP/project/filters/Filter.java
+++ project/src/main/java/com/progettoOOP/project/filters/Filter.java
@@ -1,3 +1,4 @@
+
 package filters;
 
 import java.util.ArrayList;
@@ -5,31 +6,58 @@ import utility.FileModel;
 import exceptions.*;
 import utility.GetInfoFromJSON;
 
+/**
+ * This class is the abstract superclass from which every filter inherits
+ * @author Porfiri Pierandrea
+ * @author Staffolani Federico
+ */
 public abstract class Filter {
 	
+	/** The JSONObject */
 	private GetInfoFromJSON dropbox;
+	
+	/** The ArrayList got from the JSONObject. */
 	private ArrayList<FileModel> lista;
 	
+	/**
+	 * Instantiates a new filter.
+	 *
+	 * @throws EmptyListExc the list is empty
+	 */
 	public Filter() throws EmptyListExc
 	{
 		this.dropbox = new GetInfoFromJSON();
-		if(this.dropbox == null) 
+		this.lista = dropbox.getInfo();
+		
+		if(this.lista.toString().equals("[]")) 
 		{
 			throw new EmptyListExc("Empty JSON database");
 		}
-		else 
-		{
-			this.lista = dropbox.getInfo();
-		}
+		
 	}
 	
-	// generica funzione di filtraggio
+	/**
+	 * 
+	 * Generic abstract filtering method
+	 * 
+	 * @return the filtered ArrayList
+	 */ 
 	public abstract ArrayList<FileModel> listFilter();
 	
+	/**
+	 * Gets the ArrayList
+	 *
+	 * @return the ArrayList
+	 */
 	public ArrayList<FileModel> getLista() {
 		return lista;
 	}
 
+	/**
+	 * Sets the ArrayList
+	 *
+	 * @param lista the new ArrayList
+	 */
 	public void setLista(ArrayList<FileModel> lista) {
 		this.lista = lista;
 	}
