695fd32f60e5b906432a6c00b702d47c88cd2790
PierandreaPorfiri <91202391+PierandreaPorfiri@users.noreply.github.com>
2022-01-05 18:43:03 +0100

    Update FilterDate.java
    Added Javadoc

--- project/src/main/java/com/progettoOOP/project/filters/FilterDate.java
+++ project/src/main/java/com/progettoOOP/project/filters/FilterDate.java
@@ -1,18 +1,36 @@
 package filters;
 
 import java.util.ArrayList;
+import exceptions.EmptyListExc;
 import utility.FileModel;
 
 
+/**
+ * This class is the implementation of {@link Filter} for the filter on file's date
+ * @author Porfiri Pierandrea
+ * @author Staffolani Federico
+ */
 public class FilterDate extends Filter {
 	
-	private String data;
+	/** The date of the file or the folder */
+	private String date;
 	
-	public FilterDate (String data) {
+	/**
+	 * Instantiates a new filter date
+	 *
+	 * @param date the date
+	 * @throws EmptyListExc the list is empty 
+	 */
+	public FilterDate (String date) throws EmptyListExc {
 		super();
-		this.data = data;
+		this.date = date;
 	}
 	
+	/**
+	 * List filter.
+	 *
+	 * @return the filtered ArrayList with all files and folders containing the date
+	 */
 	public ArrayList<FileModel> listFilter()
 	{
 		
@@ -24,7 +42,8 @@ public class FilterDate extends Filter {
 			if (!(i.getTag().equals("folder"))) 
 			{
 				
-				if((this.data).equals(i.getServer_modified().substring(0, 10))) 
+				// checks if the input date is equal to the JSONObject one and checks only YYYY-MM-DD  
+				if((this.date).equals(i.getServer_modified().substring(0, 10)))
 				{
 					ListaFiltrata.add(i);
 				}
