de34fa6cb424a2c2b3a9efd3bd72f523493aaee7
Federico Staffolani <94174873+fedeStaffo@users.noreply.github.com>
2022-01-05 18:43:58 +0100

    Update StatsOnSize.java
    added javadoc

--- project/src/main/java/com/progettoOOP/project/stats/StatsOnSize.java
+++ project/src/main/java/com/progettoOOP/project/stats/StatsOnSize.java
@@ -1,29 +1,54 @@
 package stats;
 
+import exceptions.*;
 import java.util.ArrayList;
-
-import filters.FilterExt;
+import filters.*;
 import utility.FileModel;
 
+
+/**
+ * This class calculates the statistic based on the size of the files 
+ * @author Porfiri Pierandrea
+ * @author Staffolani Federico
+ */
 public class StatsOnSize {
 
-	private String ext;
+	/** The extension of the file. */
+	private String extension;
 	
-	public StatsOnSize(String extension) 
+	/** The size of the file. */
+	private Number size;
+	
+	/**
+	 * Instantiates a new statistic on size.
+	 *
+	 * @param extension the extension
+	 * @param size the size
+	 */
+	public StatsOnSize(String extension, Number size) 
 	{
-		this.ext = extension;
+		
+		this.extension = extension;
+		this.size = size; 
 	}
 
-	public void biggerfileforExt() 
+	/**
+	 * This method makes the computation of the statistic 
+	 *
+	 * @return a string containing the name and the size of the  
+	 * biggest file among the ones with the given extension
+	 * @throws EmptyListExc the list is empty
+	 */
+	public String biggestfileforExt() throws EmptyListExc 
 	{
-		FilterExt filter = new FilterExt(this.ext);
+		FilterExt filter = new FilterExt(this.extension);
 		ArrayList<FileModel> list = filter.listFilter();
 		long size = 0;
 		String name = null;
 		
 		for(FileModel x : list) 
 		{
-			if(x.getTag().equals("file"))
+			if(x.getTag().equals("file")) // to avoid working with folders that don't have a size
 			{
 				if(size <= x.getSize().longValue())
 					{
@@ -33,14 +58,25 @@ public class StatsOnSize {
 			}
 
 		}
+		// no file with the wanted extension was found
+		if(name == null) return null;
 		
-		System.out.println("The biggest " + this.ext + " file is " 
-							+ name + " which weight is " + (size*0.000125) + " kilobyte");
+		String s = "The biggest " + this.extension + " file is " 
+							+ name + " which weight is " + (size*0.000125) + " kilobyte";
+		return s;
+		
 	}
 	
-	public void smallerfileforExt() 
+	/**
+	 * This method makes the computation of the statistic
+	 *
+	 * @return a string containing the name and the size of the  
+	 * smallest file among the ones with the given extension
+	 * @throws EmptyListExc the list is empty 
+	 */
+	public String smallerfileforExt() throws EmptyListExc 
 	{
-		FilterExt filter = new FilterExt(this.ext);
+		FilterExt filter = new FilterExt(this.extension);
 		ArrayList<FileModel> list = filter.listFilter();
 		long size = 0;
 		String name = null;
@@ -55,7 +91,7 @@ public class StatsOnSize {
 						name = x.getName();
 					}
 			}
-			if(size==0 && x.getTag().equals("file")) 
+			if(size==0 && x.getTag().equals("file")) // first found file
 			{
 				size = x.getSize().longValue();
 				name = x.getName();
@@ -63,17 +99,110 @@ public class StatsOnSize {
 			
 
 		}
+		if(name == null) return null;
 		
-		System.out.println("The smallest " + this.ext + " file is " 
-							+ name + " which weight is " + (size*0.000125) + " kilobyte");
+		String s = "The smallest " + this.extension + " file is " 
+							+ name + " which weight is " + (size*0.000125) + " kilobyte";
+		return s;
 	}
 	
-	public String getExt() {
-		return ext;
+	
+	/**
+	 * This method makes the computation of the statistic
+	 *
+	 * @return a string containing the number of all the files bigger  
+	 * than the given size, divided for extension
+	 * @throws EmptyListExc the list is empty 
+	 */
+	public String higherDim() throws EmptyListExc 
+	{
+		FilterBiggerSize x = new FilterBiggerSize(this.size);
+		ArrayList<FileModel> lista = x.listFilter();
+		Integer c_pdf=0, c_xls=0, c_jpg=0, c_pptx=0, c_doc=0; //counters for the allowed extensions
+		
+		for(FileModel i : lista) 
+		{
+			switch(i.getExtension()) 
+			{
+				case ".pdf": c_pdf++; break;						
+				case ".xls": c_xls++; break;
+				case ".jpg": c_jpg++; break;
+				case ".pptx": c_pptx++; break;
+				case ".doc": c_doc++; break;
+			}
+		}
+		
+		String s = "I file con dimensione maggiore di " + this.size + " sono:\n" +  c_pdf + " pdf\n" + c_xls + " documenti excel\n"
+				+ c_jpg + " immagini jpg\n" + c_pptx + " power point\n" + c_doc + " documenti word";
+		return s;
+	}
+	
+	/**
+	* This method makes the computation of the statistic
+	 *
+	 * @return a string containing the number of all the files smaller  
+	 * than the given size, divided for extension
+	 * @throws EmptyListExc the list is empty
+	 */
+	public String lowerDim() throws EmptyListExc 
+	{
+		FilterSmallerSize x = new FilterSmallerSize(this.size);
+		ArrayList<FileModel> lista = x.listFilter();
+		
+		int c_pdf=0, c_xls=0, c_jpg=0, c_pptx=0, c_doc=0; 
+		
+		for(FileModel i : lista) 
+		{
+			switch(i.getExtension()) 
+			{
+				case ".pdf": c_pdf++; break;						
+				case ".xls": c_xls++; break;
+				case ".jpg": c_jpg++; break;
+				case ".pptx": c_pptx++; break;
+				case ".doc": c_doc++; break;
+			}
+		}
+		String s = "I file con dimensione maggiore di " + this.size + " sono:\n" +  c_pdf + " pdf\n" + c_xls + " documenti excel\n"
+				+ c_jpg + " immagini jpg\n" + c_pptx + " power point\n" + c_doc + " documenti word";
+		return s;
+	}
+	
+	
+	
+	/**
+	 * Gets the extension.
+	 *
+	 * @return the extension
+	 */
+	public String getExtension() {
+		return extension;
 	}
 
-	public void setExt(String ext) {
-		this.ext = ext;
+	/**
+	 * Sets the extension.
+	 *
+	 * @param extension the new extension
+	 */
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+
+	/**
+	 * Gets the size.
+	 *
+	 * @return the size
+	 */
+	public Number getSize() {
+		return size;
+	}
+
+	/**
+	 * Sets the size.
+	 *
+	 * @param size the new size
+	 */
+	public void setSize(Number size) {
+		this.size = size;
 	}
 	
 }
