a61ced4c7dae4bb28ca67e6b3a810b8aab6b6eea
PierandreaPorfiri <91202391+PierandreaPorfiri@users.noreply.github.com>
2022-01-05 18:43:38 +0100

    Update FilterExt.java
    Added Javadoc

--- project/src/main/java/com/progettoOOP/project/filters/FilterExt.java
+++ project/src/main/java/com/progettoOOP/project/filters/FilterExt.java
@@ -1,24 +1,40 @@
 package filters;
 
 import java.util.ArrayList;
+import exceptions.EmptyListExc;
 import utility.FileModel;
-//import utility.GetInfoFromJSON;
 
+
+/**
+ * This class is the implementation of {@link Filter} for the filter on file's extension
+ * @author Porfiri Pierandrea
+ * @author Staffolani Federico
+ */
 public class FilterExt extends Filter {
 	
+	/** The extension of the file. */
 	private String estensione;
 	
-	public FilterExt (String ext)
+	/**
+	 * Instantiates a new filter extension
+	 *
+	 * @param extension the extension
+	 * @throws EmptyListExc the list is empty 
+	 */
+	public FilterExt (String extension) throws EmptyListExc
 	{
 		super();
-		this.estensione = ext;
+		this.estensione = extension;
 	}
 
+	/**
+	 * List filter.
+	 *
+	 * @return the filtered ArrayList with all the files containing the extension
+	 */
 	@Override
 	public ArrayList<FileModel> listFilter() {
 		
-		//GetInfoFromJSON x = new GetInfoFromJSON();
-		//ArrayList<FileModel> ListaFile = x.getInfo();
 		ArrayList<FileModel> ListaFiltrata = new ArrayList<FileModel>();
 		
 		for (FileModel i : this.getLista()) 
