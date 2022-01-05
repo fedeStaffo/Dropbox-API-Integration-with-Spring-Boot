a48a1b0ed753c28d65dfda7b22e8b54f04b096ce
PierandreaPorfiri <91202391+PierandreaPorfiri@users.noreply.github.com>
2022-01-05 18:44:53 +0100

    Update FilterPath.java
    Added Javadoc

--- project/src/main/java/com/progettoOOP/project/filters/FilterPath.java
+++ project/src/main/java/com/progettoOOP/project/filters/FilterPath.java
@@ -1,23 +1,40 @@
 package filters;
 
 import java.util.ArrayList;
-
+import exceptions.EmptyListExc;
 import utility.FileModel;
 
+/**
+ * This class is the implementation of {@link Filter} for the filter on file's path
+ * @author Porfiri Pierandrea
+ * @author Staffolani Federico
+ */
 public class FilterPath extends Filter {
 
+	/** The path of the file */
 	private String path;
 	
-	public FilterPath(String p) 
+	/**
+	 * Instantiates a new filter path.
+	 *
+	 * @param path the path
+	 * @throws EmptyListExc the list is empty 
+	 */
+	public FilterPath(String path) throws EmptyListExc 
 	{
-		super(); // richiama il costruttore di Filter
-		this.path = p;
+		super();  // to call the constructor of the abstract class Filter
+		this.path = path;
 	}
 	
-	// metodo per filtrare i file e le cartelle per iniziale del nome
+	/**
+	 * List filter.
+	 *
+	 * @return the filtered ArrayList with all the files containing the path
+	 */
+	
 	@Override
 	public ArrayList<FileModel> listFilter() {
-		// TODO Auto-generated method stub
+		
 		
 		ArrayList<FileModel> lista_filtrata = new ArrayList<FileModel>();
 		
@@ -25,10 +42,9 @@ public class FilterPath extends Filter {
 		{
 			if(!(x.getTag().equals("folder"))) 
 			{
-				int pos = x.getPath().lastIndexOf("/");
-				String path_cut = x.getPath().substring(0, pos);
-				//System.out.println(pos);
-				//System.out.println(path_cut);
+				int pos = x.getPath().lastIndexOf("/"); // gets the position of the last folder separator
+				
+				String path_cut = x.getPath().substring(0, pos); // gets the path without file's name
 				
 				if(path_cut.equals(this.path)) 
 				{
