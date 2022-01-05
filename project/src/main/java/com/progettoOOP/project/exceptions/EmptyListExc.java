dfee2ee80950913c0ab661e10fb64d2bc2fb709c
Federico Staffolani <94174873+fedeStaffo@users.noreply.github.com>
2022-01-05 18:44:50 +0100

    Update EmptyListExc.java
    added javadoc

--- project/src/main/java/com/progettoOOP/project/exceptions/EmptyListExc.java
+++ project/src/main/java/com/progettoOOP/project/exceptions/EmptyListExc.java
@@ -1,18 +1,31 @@
+
 package exceptions;
 
-public class EmptyListExc extends Exception {
 
+/**
+ * Exception thrown if the list of files is empty
+ * @author Porfiri Pierandrea
+ * @author Staffolani Federico
+ */
+public class EmptyListExc extends Exception{
+
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 	
-	public EmptyListExc() 
-	{
+	/**
+	 * Instantiates a new empty list exception.
+	 */
+	public EmptyListExc(){
 		super();
 	}
-	
-	public EmptyListExc(String msg) 
-	{
+
+	/**
+	 * Instantiates a new empty list exception.
+	 *
+	 * @param msg message of error shown
+	 */
+	public EmptyListExc(String msg){
 		super(msg);
 	}
-
 	
 }
