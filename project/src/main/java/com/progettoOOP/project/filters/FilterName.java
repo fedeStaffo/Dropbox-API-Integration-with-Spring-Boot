6c141fd8fb772875b22397c92ff97dcad8df439e
PierandreaPorfiri <91202391+PierandreaPorfiri@users.noreply.github.com>
2022-01-05 18:44:13 +0100

    Update FilterName.java
    Added Javadoc

--- project/src/main/java/com/progettoOOP/project/filters/FilterName.java
+++ project/src/main/java/com/progettoOOP/project/filters/FilterName.java
@@ -1,29 +1,47 @@
 package filters;
 
 import java.util.ArrayList;
-
+import exceptions.EmptyListExc;
 import utility.FileModel;
 
+/**
+ * This class is the implementation of {@link Filter} for the filter on file's name
+ * @author Porfiri Pierandrea
+ * @author Staffolani Federico
+ */
 public class FilterName extends Filter{
 
+	/** The first letter of the file's name */
 	private char first_lett;
 	
-	public FilterName(char letter) 
+	/**
+	 * Instantiates a new filter name.
+	 *
+	 * @param letter the letter
+	 * @throws EmptyListExc the list is empty 
+	 */
+	public FilterName(char letter) throws EmptyListExc 
 	{
-		super(); // richiama il costruttore di Filter
+		super(); 
 		this.first_lett = letter;
 	}
 	
-	// metodo per filtrare i file e le cartelle per iniziale del nome
+	/**
+	 * List filter.
+	 *
+	 * @return the filtered ArrayList with all the files starting with the letter
+	 */
+	
 	@Override
 	public ArrayList<FileModel> listFilter() {
-		// TODO Auto-generated method stub
+	
 		
 		ArrayList<FileModel> lista_filtrata = new ArrayList<FileModel>();
 		
 		for(FileModel x : this.getLista()) 
 		{
-			if(x.getName().charAt(0) == this.first_lett)
+			// checks if the first letter of file's name is equal to the letter in input
+			if(x.getName().charAt(0) == (this.first_lett))
 			{
 				lista_filtrata.add(x);
 			}
