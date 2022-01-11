8bae528772f68bddddad7e289d8e3cfeb3b88208
Federico Staffolani <94174873+fedeStaffo@users.noreply.github.com>
2022-01-11 17:27:00 +0100

    Moved tests files

---
+++ project/src/test/java/tests/ProjectApplicationTests.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * The Class ProjectApplicationTests.
+ * @author Porfiri Pierandrea
+ * @author Staffolani Federico
+ */
+@SpringBootTest
+class ProjectApplicationTests {
+
+	/**
+	 * Context loads.
+	 */
+	@Test
+	void contextLoads() {
+	}
+
+}
