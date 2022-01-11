b9f04cb004b7ba30d87834c6167393ffd3f7e07c
PierandreaPorfiri <91202391+PierandreaPorfiri@users.noreply.github.com>
2022-01-11 17:49:53 +0100

    Delete project/doc directory

--- project/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.progettoOOP.project"},{"l":"exceptions"},{"l":"filters"},{"l":"stats"},{"l":"utility"}];updateSearchResults();
